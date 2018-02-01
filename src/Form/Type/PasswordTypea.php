5aaf2e063ef860c2857707c68010137203969062
David Meimoun <david.meimounhotmail.fr>
2018-02-01 18:39:39 +0100

    DM : Ajout de la possibilité de modifier son mot de passe pour un USER et changement de mot de passe lors de la premiere connexion

---
+++ src/Form/Type/PasswordTypea.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace GestionnaireLivret\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordTypea extends AbstractType
+{
+    private $teachers;
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->teachers = $options['teacher_choices'];
+        
+        $builder        
+            ->add('id', ChoiceType::class, [
+                'label' => 'Teacher',
+                'mapped' => true,
+                'choices' => $this->teachers,
+            ])
+            ->add('password', RepeatedType::class, array(
+                'type'            => PasswordType::class,
+                 'invalid_message' => 'The password fields must match.',
+                 'options'         => array('required' => true),
+                 'first_options'   => array('label' => 'Password'),
+                 'second_options'  => array('label' => 'Repeat password'),
+             ));
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+        'teacher_choices' => null,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
